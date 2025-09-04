444e73bb84d370d7ca66d4c5ef82dd28d621c80d
reena-obc <reena.telasi@obconnect.io>
2025-09-05 01:37:40 +0530

    feat: create server,router,controller, mongodb connection and login stream a/c

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+export async function signup(req, res) {
+    res.send("signup router")
+}
+export async function login(req, res) {
+    res.send("login router")
+}
+export async function logout(req, res) {
+    res.send("logout router")
+}
+
---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import "dotenv/config"
+import authRoutes from './routes/auth.route.js'
+import { connectDB } from './lib/db.js'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.use('/api/auth', authRoutes)
+app.listen(PORT, () => {
+    console.log(`server is running at port ${PORT}`)
+    connectDB()
+})
